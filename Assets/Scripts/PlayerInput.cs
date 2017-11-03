dc6d32ee34ce270a8da7b5a21caed1d53288054c
= <jsm1505104@gmail.com>
2017-11-04 00:13:22 +0900

    M

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour {
 	public Vector3 dir;
 	public Player pActor;
+	public bool inputA = false;
+	public bool inputS = false;
+	public bool inputD = false;
+	public bool inputQ = false;
+
 	// Update is called once per frame
 	void Update () {
 		dir.x = Input.GetAxis ("Horizontal");
@@ -12,5 +17,9 @@ public class PlayerInput : MonoBehaviour {
 		dir = dir.normalized;		
 		if (dir != Vector3.zero)
 			pActor.Move (dir);
+		inputA = (Input.GetKey ("a")||Input.GetKey("joystick button 0")) ? true : false;
+		inputS = (Input.GetKey ("s")||Input.GetKey("joystick button 1")) ? true : false;
+		inputD = (Input.GetKey ("d")||Input.GetKey("joystick button 3")) ? true : false;
+		inputQ = (Input.GetKey ("q")||Input.GetKey("joystick button 2")) ? true : false;
 	}
 }
