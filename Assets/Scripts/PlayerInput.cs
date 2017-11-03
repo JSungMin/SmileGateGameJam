c8fd83c41fb3d438e36cc171a6584e466a89f537
= <jsm1505104@gmail.com>
2017-11-03 23:17:32 +0900

    M

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
+	public Rigidbody pRigid;
+	public Vector3 dir;
+
+	void OnEnable ()
+	{
+		if (null == pRigid)
+			Debug.LogError ("Rigidbody is Null");
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +19,10 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		dir.x = Input.GetAxis ("Horizontal");
+		dir.y = Input.GetAxis ("Vertical");
+		dir = dir.normalized;
+
+
 	}
 }
