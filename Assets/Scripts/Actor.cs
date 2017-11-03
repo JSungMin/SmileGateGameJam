9f0ebbc2319dd149788f54ecc9224ce6bddb7e83
= <jsm1505104@gmail.com>
2017-11-04 01:20:04 +0900

    Actor Updated

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
 
 [System.Serializable]
 public class ActorInfo
@@ -15,13 +16,27 @@ public class Actor : MonoBehaviour {
 	public ActorInfo acInfo;
 	public Rigidbody rigid;
     public WeaponInfo weInfo;
+	public SkeletonAnimation skel;
+	public string curAnimation;
 
 	protected void OnEnable ()
 	{
 		rigid = GetComponent<Rigidbody> ();
+		skel = GetComponentInChildren<SkeletonAnimation> ();
 	}
 	public virtual void Move (Vector3 dir)
 	{
 		transform.Translate (dir * acInfo.speed * Time.deltaTime);
+	}
+	void SetAnimation(int index, string name, bool loop, float time)
+	{
+		if(curAnimation == name)
+		{
+			return;
+		}else
+		{
+			skel.state.SetAnimation(index, name, loop).TimeScale = time;
+			curAnimation = name;
+		}
 	}
 }
