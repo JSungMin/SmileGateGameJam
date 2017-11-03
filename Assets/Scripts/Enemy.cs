9fff3cccdd806b1ce544d5ef5dfd253eec5f6636
JinsungGwag <jspartner1@naver.com>
2017-11-04 02:33:36 +0900

    Add Observer, Distance

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,5 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : Actor {
-    
+
+    public int score;
+    public float disToPlayer;
+    public void UpdateDistanceToPlayer()
+    {
+        float disX = Player.instance.transform.position.x - transform.position.x;
+        float disY = Player.instance.transform.position.y - transform.position.y;
+        
+        disToPlayer = Mathf.Sqrt(Mathf.Pow(disX, 2) + Mathf.Pow(disY, 2));
+    }
+
+    public delegate void InteractFunc ();
+    public event InteractFunc OnDamaged;
+
+    public void Damaged(float val)
+    {
+        acInfo.hp -= val;
+        if (null != OnDamaged.Method)
+        {
+            OnDamaged.Invoke();
+        }
+    }
+
 }
---
+++ Assets/Scripts/GameObserver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObserver : MonoBehaviour {
+
+    public static GameObserver instance;
+    public static GameObserver GetInstance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                instance = GameObject.FindObjectOfType<GameObserver>();
+                if (null == instance)
+                    Debug.LogError("Can't Find GameObserver");
+            }
+            return instance;
+        }
+    }
+
+    public List<Enemy> enemiesList = new List<Enemy>();
+
+    void Update()
+    {
+        for(int i = 0; i < enemiesList.Count; i++)
+        {
+            enemiesList[i].UpdateDistanceToPlayer();
+            Debug.Log(i + ": " + enemiesList[i].disToPlayer);
+        }
+    }
+
+}
